9721db2aafe3cadef05a1b4ccb53744e19919b72
bicknicktick <150901940+bicknicktick@users.noreply.github.com>
2025-03-27 23:08:35 +0700

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@
   "authorized_users": [
     {
       "telegram_id": "7788037823",
-      "added_at": "2023-10-25T12:00:00Z",
+      "added_at": "2099-10-25T12:00:00Z",
       "status": "active"
     }
   ],
